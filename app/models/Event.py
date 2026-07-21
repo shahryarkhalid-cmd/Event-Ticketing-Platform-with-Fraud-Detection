69f86ac6794d7eb5ad2fcf9ad85e20601597b2dc
shahryarkhalid-cmd <sherrykhalid86@gmail.com>
2026-07-21 23:39:53 +0500

    Add Event Model

---
+++ app/models/Event.py
@@ -0,0 +1,22 @@
+# app/models/event.py
+from sqlmodel import SQLModel, Field
+from typing import Optional
+from datetime import datetime, timezone
+from pydantic import BaseModel
+
+class Event(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    description: Optional[str] = None
+    venue: str
+    date: datetime
+    organizer_id: int = Field(foreign_key="user.id")
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    
+    
+class EventCreate(BaseModel):
+    name: str
+    description: Optional[str] = None
+    venue: str
+    date: datetime
+
