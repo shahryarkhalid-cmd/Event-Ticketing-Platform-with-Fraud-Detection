2debe54a5765b09d06737731d5b444f1aec883af
shahryarkhalid-cmd <sherrykhalid86@gmail.com>
2026-08-06 12:58:26 +0500

    Change in CORS

--- app/main.py
+++ app/main.py
@@ -70,6 +70,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:3000",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
     "https://tixora-lemon.vercel.app"
 ],
     allow_credentials=True,
